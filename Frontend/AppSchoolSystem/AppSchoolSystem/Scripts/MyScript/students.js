66c40e0f0a15d1d6c39adf1888f9fab3d9a6ad71
dhani1803 <dhani.s.wibowo@gmail.com>
2017-01-26 01:24:57 +0700

    add Delete function

--- Frontend/AppSchoolSystem/AppSchoolSystem/Scripts/MyScript/students.js
+++ Frontend/AppSchoolSystem/AppSchoolSystem/Scripts/MyScript/students.js
@@ -1,29 +1,23 @@
 ﻿$(document).ready(function () {
-   
+    var studentId = studentId;
     LoadStudents();
+    fDelete(studentId);
 
-    $("#detailsId").click(function () {
-        var id = $(this).$('myform').$('details').val();
-        console.log(id);
-        alert(id);
-        alert(buttonValue);
+    $('#detail').click(function(){
+        SearchStudents(id);
+        consolo.log(id);
+        });
+
+    $("body").on('submit', '#detail', function (e) {
+        e.preventDefault();
+        SearchStudents(id);
     });
-
     //Save Students
     $("body").on('submit', '#saveForm', function (e) {
         e.preventDefault();
-        SaveStudents();    
+        console.log(id);
+        SaveStudents();
     });
-
-    //Delete Students
-    $('body').on('submit', '#deleteForm', function (e) {
-        e.preventDefault();
-        DeleteStudents();
-    });
-
-    function redirect() {
-        window.location.href = '/Students/index';
-    }
 });
 
 
@@ -54,7 +48,7 @@ function LoadStudents() {
             
             if (count > 0) {                
                 var $data = $("<table id='example1'></table>").addClass("table table-bordered table-striped");
-                var header = "<thead><tr><th>NIS</th><th>Nama</th><th>Kelas</th><th>Jenis Kelamin</th><th></th></tr></thead>";
+                var header = "<thead><tr><th>NIS</th><th>Nama</th><th>Kelas</th><th>Jenis Kelamin</th><th>Action</th></tr></thead>";
                 $data.append(header);
                 console.log(" data " + count);
                 
@@ -66,7 +60,7 @@ function LoadStudents() {
                         $row.append($('<td/>').html(d.students[i].kelas));
                         $row.append($('<td/>').html(d.students[i].jenisKelamin));
                         //$row.append($('<td/>').html("<a href='/Students/Edit/" + d.students[i]._id + "' class='popup'>Edit</a>&nbsp;|&nbsp;<a href='http://localhost:8383/api/students/delete/" + d.students[i]._id + "'>Delete</a>"));
-                        $row.append($('<td/>').html("<form name='myform' id='detailsId' action='students/details' method='get'><button id='detailsId' name='details' type='submit' value='" + d.students[i]._id + "'>Detail</button></form>"));
+                        $row.append($('<td/>').html("<button type='submit' class='btn btn-default' id='detail' onclick=SearchStudents('" + d.students[i]._id + "'>Detail</button><button onclick=fDelete('" + d.students[i]._id + "') class='btn btn-default'>Delete </button> "));
                         $data.append($row);
                     }
                     
@@ -82,24 +76,21 @@ function LoadStudents() {
             alert('Error! Please try again.');
         }
     });
+
+
 }
 
 //Save Contact
 function SaveStudents() {
-    //Validation
-    if ($('#nis').val().trim()==='' ||
-        $('#nama').val().trim()=== '' ||
-        $('#kelas').val().trim() === '' ||
-        $('#jk').val().trim() === '') {
-        return false;
-    }
+
 
     var students = {
-        nis: $('#nis').val()==='' ? '0' : $('#nis').val(),
+        nis: $('#nis').val()=='' ? '0' : $('#nis').val(),
         nama: $('#nama').val().trim(),
         kelas: $('#kelas').val().trim(),
         jk: $('#jk').val().trim()
     };
+    console.log(students);
 
     students.__RequestVerificationToken = $('input[name=__RequestVerificationToken]').val();
     //Save Contact
@@ -109,52 +100,68 @@ function SaveStudents() {
         data: students,
         dataType: 'json',
         success: function (data) {
-            if (data.status) {
-                $('#nis').val('');
-                $('#nama').val('');
-                $('#kelas').val('');
-                $('#jk').val('');
-            }
+            console.log('Saved');
             window.location.href = '/Students/index';
         }
     });
 }
 
-function DeleteStudents() {
+
+//Delete Students
+function fDelete(deleteId) {
+    //event.preventDefault();
+    console.log("Deletting record");
+    console.log(deleteId);
+    var studentId = {
+        id: deleteId
+    };
+
+    var form = $(this);
+
+    console.log(studentId);
     $.ajax({
+        //url: 'http://192.168.1.29:3000/delete',
         url: 'http://localhost:8383/api/students/delete',
-        type: 'POST',
+        data: studentId,
+        type: 'post',
         dataType: 'json',
-        data: {
-            'id': $('#id').val(),
-            '__RequestVerificationToken': $('input[name=__RequestVerificationToken]').val()
-        },
-        success: function (data) {
-            alert(data.message);
-            if (data.status) {
-                $dialog.dialog('close');
-            }
-        },
-        error: function () {
-            $('#msg').html('<div class="failed">Error ! Please try again.</div>');
-        }
-    });
+        crossDomain: true
+
+    })
+    .always(function (result) {
+        console.log("From server...");
+
+    })
+    .done(function (result) {
+        console.log("Successfuly delete...");
+        LoadStudents();
+    })
+    .fail(function (xhr, result, error) {
+        console.log(error);
+
+    })
 }
+
 
 function SearchStudents(id) {
+    console.log(id);
+    window.location.href = '/Students/Details';
+    var studentI = {
+        id: id
+    };
+
     $.ajax({
-        url: 'http://localhost:8383/api/students/delete/'+id,
-        type: 'POST',
+        url: 'http://localhost:8383/api/students/search',
+        type: 'put',
         dataType: 'json',
         data: {
-            'id': $('#id').val(),
+            id: $('#id').val(),
             '__RequestVerificationToken': $('input[name=__RequestVerificationToken]').val()
         },
         success: function (data) {
             alert(data.message);
             if (data.status) {
                 $dialog.dialog('close');
-                LoadContacts();
             }
         },
         error: function () {
@@ -162,9 +169,3 @@ function SearchStudents(id) {
         }
     });
 }
-
-$(function () {
-    $.getJSON('/api/rest/abc', function (data) {
-        console.log(data);
-    });
-});
